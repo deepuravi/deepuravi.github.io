895a4ab6fc383d0955d565fd9b7ddf6211255a3a
gautamjeyaraman <gautam.jeyaraman@tataatsu.com>
2015-04-01 01:20:11 +0530

    Added sub menus

---
+++ ocu/static/js/webapp.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+    $(".activitiesNav").mouseover(function(){
+        $(".activities-nav").show();
+    });
+
+    $(".activitiesNav").mouseout(function(){
+        $(".activities-nav").hide();
+    });
+
+    $(".programNav").mouseover(function(){
+        $(".program-nav").show();
+    });
+
+    $(".programNav").mouseout(function(){
+        $(".program-nav").hide();
+    });
+
+});
