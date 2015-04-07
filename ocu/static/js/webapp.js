102cb3e3a9d11ed9950e421bc915277c73b3c245
gautamjeyaraman <gautam.jeyaraman@tataatsu.com>
2015-04-08 02:32:18 +0530

    Second cut changes

--- ocu/static/js/webapp.js
+++ ocu/static/js/webapp.js
@@ -16,4 +16,30 @@ $(document).ready(function(){
         $(".program-nav").hide();
     });
 
+    var currentMonth = moment().format('YYYY-MM');
+    var nextMonth    = moment().add('month', 1).format('YYYY-MM');
+    var events = [
+        { date: currentMonth + '-' + '10', title: 'Conference', location: 'Library' },
+        { date: currentMonth + '-' + '19', title: 'Meeting', location: 'Central Park' },
+        { date: currentMonth + '-' + '23', title: 'Demonstration', location: 'Computer center' },
+        { date: nextMonth + '-' + '07',    title: 'Photo Session', location: 'Library' }
+    ];
+
+    $('#mini-clndr').clndr({
+        template: $('#mini-clndr-template').html(),
+        events: events,
+        clickEvents: {
+           click: function(target) {
+               if(target.events.length) {
+                   var daysContainer = $('#mini-clndr').find('.days-container');
+                   daysContainer.toggleClass('show-events', true);
+                   $('#mini-clndr').find('.x-button').click( function() {
+                       daysContainer.toggleClass('show-events', false);
+                   });
+               }
+           }
+        },
+        adjacentDaysChangeMonth: true,
+        forceSixRows: true
+    });
 });
